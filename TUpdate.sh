0bf280e7a4706042eaeb159d402f825cc85e5288
hadeer youssef <hadeer.youssef.ahmed.ahmed@gmail.com>
2023-08-10 15:12:35 +0300

    added updated table

--- TUpdate.sh
+++ TUpdate.sh
@@ -1,11 +1,11 @@
 # sed '1d' user |cut -d ":" -f 1
-
-get_col(){
+declare line 
+get_row(){
         read -p "Your new DB: " mydb
 
          read -p "Your new TN: " tableName
 
-         read -p "Your new PK: " S_id
+         read -p "Your  PK to get the record : " S_id
 
     fieldNumber=`awk -v RS=':' "/id/"'{print NR}' Databases/$mydb/$tableName`
     pksValues=`sed '1d' Databases/$mydb/$tableName |cut -d ":" -f $fieldNumber `
@@ -19,15 +19,17 @@ get_col(){
             # Check if a matching line was found
                 if [[ -n "$line" ]]; then
                 echo  $line
+                                 # if line or table empty return table is empty
+
                 break
 
                 else
                 echo "No matching line found for ID: $S_id"
-                get_col
+                get_row
                 fi
 
         fi
     done
 }
 
-get_col
+get_row
--- UpdateT.sh
+++ UpdateT.sh
@@ -1,32 +1,45 @@
 #!/usr/bin/bash
-echo "Update Record in Table"
-read -p "Enter Database name " DBname
-read -p "Enter table name you want to update : " tableName
+function updateTable {
+      read -p "Enter DB name you want to update: " mydb
 
-selectcolumn
-
-selectcolumn(){
-    read -p "Enter column you want to update : " col
-    read -p "Enter the new data : " newdata
-    s=$(sed -n "/$col/p" "Databases/$DBname/$tableName")
-        if [[ $col ]] && [[ $s ]];then
-
-            echo "select $col from $tableName"
-            col=$col newdata=$newdata
-            awk -v '
-            BEGIN{FS=":" ; col=col ; newdata=newdata ; y=0}
-            {
-            i=1;
-            while(i<=NF){
-                if($i == $col){
-                    $i=newdata;
-                }
-                i++
-            }' "Databases/$DBname/$tableName"
-    
+  read -p "Enter table name you want to update: " Tname
+  
+  echo -e "Enter  Column name: "
+  read -p "Enter Field name: "
+  #This line uses awk to search for column name in the first line
+  fid=$(awk 'BEGIN{FS=":"}{if(NR==1){for(i=0;i<=NF;i++){if($i=="'$field'") print i}}}' Databases/$mydb/$Tname)
+  if [[ $fid == "" ]]
+  then
+    echo "Not Found"
+    tablesMenu
+  else
+  #This line uses awk to search for rows in the table
+    echo -e "Enter Value: "
+    read -p "Enter your Value: " val
+    result=$(awk 'BEGIN{FS=":"}{if ($'$fid'=="'$val'") print $'$fid'}' Databases/$mydb/$Tname 2>>./.error.log)
+    if [[ $result == "" ]]
+    then
+      echo "Value Not Found"
+      tablesMenu
     else
-        echo "coloumn not found"
-        selectcolumn
+      echo -e "Enter field name to set: "
+      read -p "Enter field set: "setField
+      setFid=$(awk 'BEGIN{FS=":"}{if(NR==1){for(i=1;i<=NF;i++){if($i=="'$setField'") print i}}}' Databases/$mydb/$Tname)
+      if [[ $setFid == "" ]]
+      then
+        echo "Not Found"
+        tablesMenu
+      else
+        echo -e "Enter new Value to set: "
+        read -p"Enter New Value: "newValue
+        NR=$(awk 'BEGIN{FS=":"}{if ($'$fid' == "'$val'") print NR}' Databases/$mydb/$Tname 2>>./.error.log)
+        oldValue=$(awk 'BEGIN{FS=":"}{if(NR=='$NR'){for(i=1;i<=NF;i++){if(i=='$setFid') print $i}}}' Databases/$mydb/$Tname 2>>./.error.log)
+        echo $oldValue
+        sed -i ''$NR's/'$oldValue'/'$newValue'/g' Databases/$mydb/$Tname 2>>./.error.log
+        echo "Row Updated Successfully"
+        tablesMenu
+      fi
     fi
-
+  fi
 }
+updateTable
